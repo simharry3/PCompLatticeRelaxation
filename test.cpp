23e19e31309eabac482130813ed98213dca12dae
BookerSeaFood <sithjordan@gmail.com>
2015-05-11 11:07:19 -0400

    Bop

--- test.cpp
+++ test.cpp
@@ -116,8 +116,6 @@ void print(double* a){
 int main(void){
   //Serial Code:
     clock_t tS;
-    clock_t tP;
-    clock_t tT;
     clock_t tO;
     tS = clock();
     double* notPlate_s = new double[N]; //Serial copy of the non-plate locations
@@ -128,57 +126,7 @@ int main(void){
         notPlate_s = tempVal_s;
     }
     tS = clock() - tS;
-
-
-    tP = clock();
-  //PARALLEL CODE:
-
-  double *notPlate_h; //Host copy of the non-plate locations
-  double *notPlate_d; //Device copy of the non-plate locations
-  int size = N * sizeof(double);
   
-  //The first thing we need to do is fill the initial experiment vector:
-  cudaMalloc((void **)&notPlate_d, size); //Allocate memory on device for the not-plate vector
-  notPlate_h = (double *)malloc(size); //Allocate memory on the host for the not-plate vector
-  cudaMemcpy(notPlate_d, notPlate_h, size, cudaMemcpyHostToDevice);
-  fill<<<N,1>>>(notPlate_d);
-  cudaMemcpy(notPlate_h, notPlate_d, size, cudaMemcpyDeviceToHost);
-
-
-  //Now we need to create a secondary vector to hold our rolling values while the GPU works:
-  double *tempVal_h;
-  double *tempVal_d;
-  
-  cudaMalloc((void **)&tempVal_d, size); //Allocate memory on the device for the temp vector
-  tempVal_h = (double *)malloc(size); //allocate memory on the host for the temp vector
-  
-  
-  //Since GPU time is cheap, we don't set a threshold, just run the simulation 1000 times:
-  for(int i = 0; i < 1000; ++i){
-    cudaMemcpy(notPlate_d, notPlate_h, size, cudaMemcpyHostToDevice);
-    average<<<N,1>>>(notPlate_d, tempVal_d);
-    cudaMemcpy(tempVal_h, tempVal_d, size, cudaMemcpyDeviceToHost);
-    notPlate_h = tempVal_h;
-  }
-  //The output here will be redirected to a plaintext file which will then be opened and plot in OCTAVE
-  //print(notPlate_h);
-
-  //When uncommented, this prints to the std output what the values at the user input points are:  
- /* while(true){
-    std::cout << "\nPlease enter an X coordinate: ";
-    double x; 
-    std::cin >> x;
-    x = x*U;
-    std::cout << "\nPlease enter a Y coordinate: ";
-    double y;
-    std::cin >> y;
-    y = y*U;
-    std::cout << "The point (" << x/U << "," << y/U << ") has value: " << notPlate_h[(int)(y*W+x)] << " Statvolts\n";
-  }*/
-  //Free the memory just to be safe:
-  free(notPlate_h);
-  cudaFree(notPlate_d);
-  tP = clock() - tP;
 
   tO = clock();
 
@@ -196,26 +144,7 @@ int main(void){
   }
   tO = clock() - tO;
 
-  tT = clock();
-
-  /*//THRUST CODE
-  thrust::device_vector<double> notPlate_dT(N);
-  thrust::host_vector<double> notPlate_hT(N);
-  thrust::generate(notPlate_dT.begin(), notPlate_dT.end(),fillSerial());
-  thrust::copy(notPlate_dT.begin(), notPlate_dR.end(), notPlate_hT.begin());
-
-  thrust::host_vector<double> tempVal_hT(N);
-  thrust::device_vector<double> tempVal_dT(N);
-
-  for(int i = 0; i < 1000; ++i) {
-    thrust::copy(notPlate_dT.begin(), notPlate_dT.end(), notPlate_hT.begin());
-    thrust::transform(notPlate_dTbegin(), notPlate_dT.end(), tempVal_dT.begin(), tempVal_dT.begin(), averageSerial());
-    thrust::copy(notPlate_hT.begin(), notPlate_hT.end(), notPlate_dT.begin());
-    notPlate_hT = tempVal_hT;
-  }*/
-
-  tT = clock() - tT;
-  std::cout << "N: " << N << " | SERIAL: " << (float)tS/CLOCKS_PER_SEC << "s | PARALLEL: " << (float)tP/CLOCKS_PER_SEC << "s | OPENMP: " << (float)tO/CLOCKS_PER_SEC << "s | THRUST: ";
-  std::cout << (float)tT/CLOCKS_PER_SEC << "s" << std::endl;
+  
+  std::cout << "N: " << N << " | SERIAL: " << (float)tS/CLOCKS_PER_SEC << "s | OPENMP: " << (float)tO/CLOCKS_PER_SEC << "s" << std::endl;
   return 0;
 }
